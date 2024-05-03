5d88df69de3b1d9eeb3189a49b73211006fbbed6
JakeWendling <jakewendling57@gmail.com>
2024-05-03 02:35:20 +0000

    api and model done

---
+++ ProjectFinal/api.py
@@ -0,0 +1,43 @@
+import transformers
+from flask import Flask, request
+from transformers import AutoTokenizer
+from transformers import pipeline
+
+# import torch
+import numpy as np
+
+model = transformers.TFAutoModelForSequenceClassification.from_pretrained("models/diagnosis_model")
+checkpoint = "distilbert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint, use_fast=True)
+model_input = pipeline(task="text-classification", model= model, tokenizer=tokenizer)
+
+app = Flask(__name__)
+
+@app.route('/models/diagnosis_model/v1', methods=['GET'])
+def model_info():
+   return {
+      "version": "v1",
+      "name": "diagnosis_model",
+      "description": "Classify text containing symptoms (one of 22 diagnoses)",
+      "pretrained_model": "distilbert-base-uncased"
+   }
+
+@app.route('/models/diagnosis_model/v1', methods=['POST'])
+def classify_clothes_image():
+   text = request.json.get('text')
+   if not text:
+      return {"error": "The 'text' field is required"}, 404
+   result = model_input(text)[0]
+   return postprocess(result)
+
+def postprocess(a):
+   """
+   Converts output into an readable output.
+   """
+   labels = ['allergy', 'arthritis', 'bronchial asthma', 'cervical spondylosis', 'chicken pox', 'common cold', 'dengue', 'diabetes', 'drug reaction', 'fungal infection', 'gastroesophageal reflux disease', 'hypertension', 'impetigo', 'jaundice', 'malaria', 'migraine', 'peptic ulcer disease', 'pneumonia', 'psoriasis', 'typhoid', 'urinary tract infection', 'varicose veins']
+   index = int(a["label"][6:])
+   return {"diagnosis":labels[index], "score": a["score"]}
+
+# start the development server
+if __name__ == '__main__':
+   app.run(debug=True, host='0.0.0.0')
---
+++ ProjectFinal/run_flask.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+docker build -t jakewendling/ml_diagnosis .
+docker run -it --rm -p 5000:5000 jakewendling/ml_diagnosis
